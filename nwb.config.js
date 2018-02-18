7740798c87164d5b5e090098857beab4b6f87a1e
codie <codiechanel@gmail.com>
2018-02-18 20:38:17 +0800

    nice

--- nwb.config.js
+++ nwb.config.js
@@ -17,18 +17,14 @@ module.exports = {
        * in order to use the Demo mode
        * which would not work if externals is set
        */
-      /**
-       * this fixes the devt version of Demo
-       * but not the dist version because this gets
-       * executed...if we could find out how nwb builds
-       * the Demo app, perhaps we could modify it there
-       */
       if (process.env.NODE_ENV === "production") {
-        console.log('we are in production' )
-        config.externals = ["react", "material-ui", "material-ui/Progress"]
+        // console.log('we are in production', config.entry["demo"] )
+        if (!config.entry["demo"]) {
+          config.externals = ["react", "material-ui", "material-ui/Progress"]
+        }
       }
       else {
-        console.log('we are in devt' )
+        console.log('we are in development mode' )
       }
       // You MUST return the edited config object
       return config
